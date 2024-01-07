052a2a98881e7a0202b0297d5d22427387e3bf58
Nguyen Tien Dung <ntd2002238@gmail.com>
2024-01-08 04:41:43 +0700

    create state first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 // import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+let stateValue;
+function useState(initState) {
+  if(stateValue ===undefined) stateValue=initState;
+  function setValue(val){
+    stateValue=val;
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+  return [stateValue, setValue];
+}
 
-import  {name, job} from "./constants/constant";
-console.log(name);
-console.log(job);
+
 function App() {
+  const [counter, setCounter]= useState(1);
+  const handleIncrement= function() {
+    setCounter(counter+1);
+  }
+  const handleDecrement= function() {
+    setCounter(counter-1);
+  }
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
 
 export default App;
+
+//create state to manage Counter
+//when state changes, UI changes
